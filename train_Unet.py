aa304c9c6a8d9a61c0b396b90814614fef490892
aparedes8 <aparedes8@gatech.edu>
2021-02-15 11:42:00 -0500

    Some preliminary setup for dataloading

---
+++ train_Unet.py
@@ -0,0 +1,15 @@
+###################################
+# Library Imports
+import tensoflow as tf
+import numpy as np
+from nibabel.testing import data_path #Used to open nifTi or .nii files
+###################################
+'''
+Dataset loader obtained from the link below
+https://medicalsegmentation.com/covid19/
+'''
+
+
+
+
+
