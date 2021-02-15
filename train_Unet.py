ac3ec741085272b5b7ee4910cfdf016ad5f55223
aparedes8 <aparedes8@gatech.edu>
2021-02-15 12:09:27 -0500

    Loaded trianing set now need to create script to download dataset to appropriate folders

--- train_Unet.py
+++ train_Unet.py
@@ -1,15 +1,25 @@
 ###################################
 # Library Imports
-import tensoflow as tf
+import tensorflow as tf
 import numpy as np
+import os
 from nibabel.testing import data_path #Used to open nifTi or .nii files
+import nibabel as nib
 ###################################
 '''
 Dataset loader obtained from the link below
 https://medicalsegmentation.com/covid19/
+
+# 100 total train images
+# resolution = 512 x 512 
 '''
+train_imgs_filename = './data/train/tr_im.nii.gz'
+train_masks_filename = './data/train/tr_mask.nii.gz'
+
+train_imgs = nib.load(train_imgs_filename) 
+train_masks = nib.load(train_masks_filename) 
 
 
-
-
-
+print(train_imgs.shape) # checking size fo dataset images
+print(train_masks.shape) # checking size of  pixelwise mask labels
+assert(train_imgs.shape == train_masks.shape)
