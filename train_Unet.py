da7a6241b0c8a5934a4bfbbb833a54b3e547ff39
aparedes8 <aparedes8@gatech.edu>
2021-02-15 23:10:30 -0500

    Got model to train it trains very poorly as of right now

--- train_Unet.py
+++ train_Unet.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 Hyperparameters
 '''
 BATCH_SIZE = 2
-EPOCHS = 5
+EPOCHS = 30
 LR = 1e-4
 
 
@@ -33,9 +33,6 @@ train_masks_nib = nib.load(train_masks_filename)
 train_imgs = train_imgs_nib.get_fdata() 
 train_masks = train_masks_nib.get_fdata() 
 
-
-# print(train_imgs.shape) # checking size of dataset images
-# print(train_masks.shape) # checking size of  pixelwise mask labels
 assert(train_imgs.shape == train_masks.shape)
 
 test_imgs_filename = './data/test/val_im.nii.gz'
@@ -44,48 +41,33 @@ test_imgs_nib = nib.load(test_imgs_filename)
 #convert nifti objects in to a numpy array
 test_imgs = test_imgs_nib.get_fdata() 
 
-# print(test_imgs.shape)
+# print(train_imgs.shape)
 assert(train_imgs.shape[0:2] == test_imgs.shape[0:2])
 
-# add channels dimension = 1 for greyscale images
-# train_imgs = np.expand_dims(train_imgs,axis=2)
-# test_imgs = np.expand_dims(test_imgs,axis=2)
-channel_sized = np.expand_dims(train_imgs,axis=2)
-INPUT_SHAPE = channel_sized.shape[0:3]
-# print(INPUT_SHAPE)
+
+# Data formatched adding channel dimension and ensuring batch is at the front index 
+train_imgs = np.transpose(train_imgs)
+train_imgs = np.expand_dims(train_imgs,axis=3)
+
+
+train_masks = np.transpose(train_masks)
+train_masks = np.expand_dims(train_masks,axis=3)
 
 ###################################
 '''
 Lets display a sample image
 '''
-sample = train_imgs[:,:,0]
+sample = train_imgs[0,:,:,0]
 
 plt.imsave("sample_ct.png",sample)
 
 ###################################
 '''
-Build crop and concat layer regular tf.keras.concate has a odd shape mismatch
-'''
-class crop_and_concat(tf.keras.layers.Layer):
-    def __init__(self):
-        super(crop_and_concat, self).__init__()
-
-    def call(self, x1,x2):
-        with tf.name_scope("crop_and_concat"):
-            x1_shape = tf.shape(x1)
-            x2_shape = tf.shape(x2)
-            # offsets for the top left corner of the crop
-            offsets = [0, (x1_shape[1] - x2_shape[1]) // 2, (x1_shape[2] - x2_shape[2]) // 2, 0]
-            size = [-1, x2_shape[1], x2_shape[2], -1]
-            x1_crop = tf.slice(x1, offsets, size)
-            return tf.concat([x1_crop, x2], 3)
-
-###################################
-'''
 Build Unet model
 '''
-def unet(input_size = INPUT_SHAPE,batch_size=BATCH_SIZE):
-    inputs = tf.keras.layers.Input(input_size,batch_size)
+def unet(input_size = (512,512,1)):
+    inputs = tf.keras.layers.Input(input_size)
+    # print(inputs.shape)
     conv1 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
     conv1 = tf.keras.layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
     pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
@@ -98,7 +80,6 @@ def unet(input_size = INPUT_SHAPE,batch_size=BATCH_SIZE):
     conv4 = tf.keras.layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = tf.keras.layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
     drop4 = tf.keras.layers.Dropout(0.5)(conv4)
-    print(drop4.shape)
     pool4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(drop4)
 
     conv5 = tf.keras.layers.Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
@@ -106,7 +87,6 @@ def unet(input_size = INPUT_SHAPE,batch_size=BATCH_SIZE):
     drop5 = tf.keras.layers.Dropout(0.5)(conv5)
 
     up6 = tf.keras.layers.Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(tf.keras.layers.UpSampling2D(size = (2,2))(drop5))
-    print(up6.shape)
     merge6 = tf.keras.layers.concatenate([drop4,up6], axis = 3)
     # merge6 = crop_and_concat()(drop4,up6)
     conv6 = tf.keras.layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
@@ -144,7 +124,8 @@ Genrate optimizer and loss function and apply them to our unet model
 '''
 opt =tf.keras.optimizers.Adam(LR)
 loss =tf.keras.losses.CategoricalCrossentropy()
-model.compile(optimizer=opt,loss=loss)
+metrics = [tf.keras.metrics.Accuracy()] 
+model.compile(optimizer=opt,loss=loss,metrics=metrics)
 
 ###################################
 '''
@@ -152,7 +133,18 @@ Train our model
 '''
 model.summary()
 
-# train_imgs = np.squeeze(train_imgs,axis=0)
-print(train_imgs.shape)
-history = model.fit(x=channel_sized,y=train_masks,batch_size=BATCH_SIZE,epochs=EPOCHS,verbose=1)
+history = model.fit(x=train_imgs,y=train_masks,batch_size=BATCH_SIZE,epochs=EPOCHS,verbose=2)
+print(history.history)
 
+results = model.evaluate(train_imgs, train_masks, batch_size=BATCH_SIZE)
+print("test loss, test acc:", results)
+
+x = np.linspace(1,len(history.history['loss'])+1,len(history.history['loss']))
+print(x)
+plt.title("Accuracy vs Loss Plot")
+plt.plot(x,history.history['loss'],label='loss',color='k')
+plt.plot(x,history.history['accuracy'],label='Accuracy',color='r')
+plt.xlabel("EPOCHS")
+plt.ylabel("Value")
+plt.legend()
+plt.savefig("./results/train_test_plot.png")
